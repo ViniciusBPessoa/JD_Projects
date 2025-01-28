1b86d840c80c6ab5dba9923ad1e1c14c3f87fd08
Vinicius Pessoa <91929358+ViniciusBPessoa@users.noreply.github.com>
2025-01-28 17:29:20 -0300

    menu em andamento

--- Solar_project/Assets/Menu/MenuController.cs
+++ Solar_project/Assets/Menu/MenuController.cs
@@ -1,14 +1,41 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // Para gerenciar cenas
 
 public class MenuController : MonoBehaviour
 {
+
+    public GameObject toActivate;
+    public GameObject toDeactivate;
+
+    // Método para iniciar o jogo (carregar uma cena específica)
     public void StartGame()
     {
+        SceneManager.LoadScene("NomeDaCenaDoJogo");
     }
 
     // Método para fechar o jogo
     public void QuitGame()
     {
+        Debug.Log("Fechando o jogo..."); // Mensagem de debug
+
+        // Fecha o jogo (funciona em builds)
         Application.Quit();
+
+        // No Editor, encerra o modo de play
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    // Método para ativar um GameObject e desativar outro
+    public void trocaMenu()
+    {
+        toActivate.SetActive(true);        
+        toDeactivate.SetActive(false);
+    }
+    public void trocaMenuInverso()
+    {
+        toActivate.SetActive(false);
+        toDeactivate.SetActive(true);
     }
 }
